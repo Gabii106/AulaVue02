13864d4de5b131f83bf2de0698d036b5a2a85548
GABRIELA SCHIRMER <gabriela_schirmer@estudante.sesisenai.org.br>
2024-11-22 21:07:56 -0300

    Aula 02

---
+++ vue-landing-page/src/components/AboutPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <section id="about" class="py-12 bg-blue-400">
+      <h2 class="text-3xl font-bold mb-4">Sobre nós</h2>
+      <p class="text-lg">Somos uma empresa fictícia criada para demonstrar as capacidades do Vue.js e MongoDB Atlas.</p>
+    </section>
+  </template>
